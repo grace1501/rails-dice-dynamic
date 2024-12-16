e3ea9688cb76354955848c29a3497caf2ee0d750
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-12-16 03:58:07 +0000

    wip - setting up

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def get_homepage
+    render({})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "home", :action => "get_homepage"})
 
+  get("/dice/2/6", { :controller => "two_six", :action => "roll_two_six"})
 end
